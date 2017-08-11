6a32ffefc4d43741bcd77c2b0b21509b17660ccd
kobe <kobe@vps-macbook-air.ad.medctr.ucla.edu>
2017-08-11 14:53:46 -0700

    updating seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,12 @@
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    time_stamp TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
 INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("Double-Double", false, now());
 INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("Numb Burger", false, now());
 INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("The Salmon Rushdie", false, now());
