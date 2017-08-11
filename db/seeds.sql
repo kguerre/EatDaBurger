72e5cf18633aa9256d99a1c081e97dd4998a7ed6
kobe <kobe@vps-macbook-air.ad.medctr.ucla.edu>
2017-08-11 11:51:08 -0700

    functioning app, completed readme

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("Double-Double", false, now());
+INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("Numb Burger", false, now());
+INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("The Salmon Rushdie", false, now());
+INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("Teddy's Biggest Burger", true, now());
+INSERT INTO burgers (burger_name, devoured, time_stamp) VALUES ("The Hamburg Burger", true, now());
